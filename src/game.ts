aa4a921976f4f38028bdd7d9e1edaffee2f3474a
Edward Drozdov <ekdrozdov@gmail.com>
2023-10-22 02:02:58 +0700

    add controller

--- src/game.ts
+++ src/game.ts
@@ -1,39 +1,125 @@
-import { DisplayObject } from "pixi.js"
+import * as PIXI from 'pixi.js'
 
-class Position {
-  x = 200
-  y = 200
-}
-
-class Agent {
-  position = new Position()
-}
-
-const moveChance = 0.1
-const avgMove = 5
-
-export function ai(agent: Agent) {
-  if (Math.random() > moveChance) return
-  const dx = Math.random() * avgMove * 2 - avgMove
-  const dy = Math.random() * avgMove * 2 - avgMove
-  agent.position.x += dx
-  agent.position.y += dy
-}
-
-interface Renderable {
+interface Point {
   x: number
   y: number
 }
 
+interface Renderable extends Point {
+  readonly id: string
+  bindTo(sceneObj: Point): void
+}
+
+class RenderableImpl implements Renderable {
+  id: string
+  public get x(): number {
+    return this._sceneObj?.x ?? 0
+  }
+  public set x(value: number) {
+    this._sceneObj && (this._sceneObj.x = value)
+  }
+  public get y(): number {
+    return this._sceneObj?.y ?? 0
+  }
+  public set y(value: number) {
+    this._sceneObj && (this._sceneObj.y = value)
+  }
+  private _sceneObj?: Point
+  constructor(id: string) {
+    this.id = id
+    this.y = 0
+  }
+  bindTo(sceneObj: Point): void {
+    this._sceneObj = sceneObj
+  }
+}
+
 interface Scene {
-  mount(x: Renderable): void
+  add(obj: Renderable): void
+  remove(obj: Renderable): void
 }
 
-class Scene {
-  agents: Agent[] = []
+interface Controller {
+  moveTo(point: Point): void
+  execute(): void
 }
 
-export const scene = new Scene()
-scene.agents.push(new Agent())
-scene.agents.push(new Agent())
-scene.agents.push(new Agent())
+interface Agent {
+  readonly renderable: Renderable
+  readonly controller: Controller
+  speed: number
+  isMoving: boolean
+}
+
+function fit(target: number, absLimit: number) {
+  if (target < 0) {
+    return Math.max(target, absLimit)
+  }
+  return Math.min(target, absLimit)
+}
+
+class ControllerImpl implements Controller {
+  private _moveTarget?: Point
+  constructor(private readonly _target: Agent) {}
+  moveTo(point: Point): void {
+    this._moveTarget = point
+    this._target.isMoving = true
+  }
+  execute() {
+    if (!this._moveTarget) return
+    // move bunny towards target with max speed available
+    // move by x
+    const { renderable } = this._target
+    const dx = fit(this._moveTarget.x - renderable.x, this._target.speed)
+    const dy = fit(this._moveTarget.y - renderable.y, this._target.speed)
+    console.log(dx)
+    renderable.x = renderable.x + dx
+    renderable.y = renderable.y + dy
+    if (
+      renderable.x === this._moveTarget.x &&
+      renderable.y === this._moveTarget.y
+    )
+      this._target.isMoving = false
+  }
+}
+
+export class Bunny implements Agent {
+  renderable: Renderable
+  controller: Controller
+  speed = 5
+  isMoving = false
+  constructor() {
+    this.renderable = new RenderableImpl('https://pixijs.com/assets/bunny.png')
+    this.controller = new ControllerImpl(this)
+  }
+}
+
+export class PixiScene implements Scene {
+  add(obj: Renderable): void {
+    const sprite = PIXI.Sprite.from(obj.id)
+    sprite.anchor.set(0.5)
+    obj.bindTo(sprite)
+    this._app.stage.addChild(sprite)
+  }
+  remove(obj: Renderable): void {
+    throw new Error('Method not implemented.')
+  }
+  constructor(private readonly _app: PIXI.Application<PIXI.ICanvas>) {}
+}
+
+// let's add an ai which will order bunny to move at
+// random point in square of 500 in case bunny is
+// not moving with a chance of 0.1.
+const moveChance = 0.1
+const moveBoxLimit = 10
+export function ai(agent: Agent) {
+  if (agent.isMoving) return
+  if (Math.random() > moveChance) return
+  const point: Point = {
+    //x: Math.floor(Math.random() * moveBoxLimit),
+    //y: Math.floor(Math.random() * moveBoxLimit),
+    x: 100,
+    y: 100,
+  }
+  agent.controller.moveTo(point)
+}
--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,22 @@
-import * as PIXI from "pixi.js"
-import { ai, scene } from "./game"
+import * as PIXI from 'pixi.js'
+import { Bunny, PixiScene, ai } from './game'
 
 const app = new PIXI.Application({
-  background: "#1099bb",
+  background: '#1099bb',
   resizeTo: window,
 })
 
 document.body.appendChild(app.view as any)
 
-// create a new Sprite from an image path
-const bunny = PIXI.Sprite.from("https://pixijs.com/assets/bunny.png")
+const scene = new PixiScene(app)
+const agent = new Bunny()
+scene.add(agent.renderable)
 
-// center the sprite's anchor point
-bunny.anchor.set(0.5)
-
-// move the sprite to the center of the screen
-bunny.x = app.screen.width / 2
-bunny.y = app.screen.height / 2
-
-app.stage.addChild(bunny)
-
-const agents = scene.agents.map((agent) =>
-  PIXI.Sprite.from("https://pixijs.com/assets/bunny.png")
-)
-agents.forEach((agent) => app.stage.addChild(agent))
+agent.renderable.x = app.screen.width / 2
+agent.renderable.y = app.screen.height / 2
 
 // Listen for animate update
 app.ticker.add((delta) => {
-  // just for fun, let's rotate mr rabbit a little
-  // delta is 1 if running at 100% performance
-  // creates frame-independent transformation
-  bunny.rotation += 0.1 * delta
-
-  scene.agents.forEach((agent) => ai(agent))
-  agents.forEach((agent, i) => {
-    agent.x = scene.agents[i].position.x
-    agent.y = scene.agents[i].position.y
-  })
+  ai(agent)
+  agent.controller.execute()
 })
