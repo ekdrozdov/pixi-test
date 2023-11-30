eb21fb479c46905e9225c19e2936aeea92ca6b8b
Eduard Drozdov <ekdrozdov@gmail.com>
2023-11-30 23:48:10 +0700

    market wip

--- src/game/market.ts
+++ src/game/market.ts
@@ -5,19 +5,19 @@ import {
 } from './goods-production/aquisition'
 import { Good, GoodTag } from './goods-production/recieps'
 
-interface Lot {
-  sell(offer: Good, want: GoodTag): void
-  buy(want: Good, offer: GoodTag): void
-}
+// interface Lot {
+//   sell(offer: Good, want: GoodTag): void
+//   buy(want: Good, offer: GoodTag): void
+// }
 
 export interface Market {
-  register(): Lot
+  register(): GoodTracker
   getPricesFor(tag: GoodTag): Good[]
 }
 
 export class MarketBase implements Market {
-  private readonly prices = new Map<GoodTag, Map<GoodTag, PriceTracker>>()
-  register(): Lot {
+  private readonly trackers = new Map<GoodTag, GoodTracker>()
+  register(): GoodTracker {
     return {
       sell: (offer: Good, want: GoodTag) => {
         this.getTracker(offer.tag, want).supply(offer.amount)
@@ -27,71 +27,75 @@ export class MarketBase implements Market {
       },
     }
   }
-  getPricesFor(tag: GoodTag): Good[] {
-    // Hit stored tag -> just return prices
-    const prices = this.prices.get(tag)
-    // Not hit stored tag -> prices are stored implicitly, build priceslist.
-    if (prices === undefined) return []
-    // this.prices
-    return []
-    throw new Error('Method not implemented.')
+  getPricesFor(tags: GoodTag[]): number[] {
+    // this.
+    // const prices = this.trackers.get(tag)
+    // if (prices)
+    //   return Array.from(prices.entries()).map<Good>(([tag, tracker]) => {
+    //     return { tag, amount: tracker.getPriceOf(tag) }
+    //   })
+    // return []
   }
 
-  private getTracker(one: GoodTag, another: GoodTag): PriceTracker {
-    // Store trackers only for the lowest key.
-    const lowest = one < another ? one : another
-    const highest = one < another ? another : one
-    const priceMap = this.prices.get(lowest)
-    if (priceMap === undefined) {
-      const map = new Map<GoodTag, PriceTracker>()
-      const tracker = new PriceTrackerBase(lowest, highest)
-      map.set(highest, tracker)
-      this.prices.set(lowest, map)
-      return tracker
-    }
-    const tracker = priceMap.get(highest)
-    if (tracker === undefined) {
-      const tracker = new PriceTrackerBase(lowest, highest)
-      priceMap.set(highest, tracker)
-      return tracker
-    }
-    return tracker
+  // TODO: tracker must track the available amount of the good in the market.
+  private getTracker(tag: GoodTag): GoodTracker {
+    const tracker = this.trackers.get(tag)
+    if (tracker) return tracker
+    const newTracker = new GoodTrackerBase(tag)
+    this.trackers.set(tag, newTracker)
+    return newTracker
+    // let oneMap = this.trackerMatrix.get(one)
+    // let tracker: GoodTracker | undefined = oneMap?.get(another)
+    // if (tracker && oneMap) return tracker
+
+    // oneMap = new Map<GoodTag, GoodTracker>()
+    // tracker = new GoodTrackerBase(one)
+    // this.trackerMatrix.set(one, oneMap)
+    // // Maybe already initialized.
+    // let anotherMap = this.trackerMatrix.get(another)
+    // if (anotherMap === undefined) {
+    //   anotherMap = new Map<GoodTag, GoodTracker>()
+    //   this.trackerMatrix.set(another, anotherMap)
+    // }
+
+    // oneMap.set(another, tracker)
+    // anotherMap.set(one, tracker)
+    // return tracker
   }
 }
 
-interface PriceTracker {
+interface GoodTracker {
   readonly price: number
-  supply(amount: number): void
-  demand(amount: number): void
+  readonly amount: number
+  supply(offer: Good, want: GoodTag): void
+  demand(want: Good, offer: GoodTag): void
   updatePrice(): void
 }
 
-class PriceTrackerBase implements PriceTracker {
+class GoodTrackerBase implements GoodTracker {
   // Represents how many sellees is buyee costs.
   price: number
+  amount: number
   private readonly basePrice: number
   private _supply = 0
   private _demand = 0
-  constructor(buyee: GoodTag, sellee: GoodTag) {
-    const buyeeBaseCost = estimateBaseProductionCost(
-      buildRequirementsTreeFor(buyee)
-    )
-    const selleeBaseCost = estimateBaseProductionCost(
-      buildRequirementsTreeFor(sellee)
-    )
-    this.basePrice = buyeeBaseCost / selleeBaseCost
+  constructor(tag: GoodTag) {
+    this.basePrice = estimateBaseProductionCost(buildRequirementsTreeFor(tag))
     this.price = this.basePrice
+    this.amount = 0
   }
   supply(amount: number): void {
     this._supply += amount
+    this.amount += amount
   }
   demand(amount: number): void {
     this._demand += amount
   }
   updatePrice(): void {
-    // TODO: consider division by 0.
     if (this._supply !== 0 && this._demand !== 0) {
       this.price = this.price * (this._demand / this._supply)
+    } else {
+      this.price = 0
     }
     const minPricePercent = 0.3
     this.price = Math.max(this.price, this.basePrice * minPricePercent)
